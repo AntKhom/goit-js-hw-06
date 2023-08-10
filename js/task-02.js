e3eecc60b262c44cb2661149c8eac021ff5b6098
AntKhom <antonhomenko1988@gmail.com>
2023-08-10 10:00:28 -0400

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+const items = [];
+
+for (let ingridient of ingredients) {
+  const liEl = document.createElement('li');
+  liEl.textContent = ingridient;
+  liEl.classList.add('item');
+  items.push(liEl);
+}
+
+console.log(...items);
+
+listEl.append(...items);
