48e34956d1d2e087993c2ab054617bee936aa075
AntKhom <antonhomenko1988@gmail.com>
2023-08-13 14:39:05 -0400

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const btn = document.querySelector('.change-color');
+const spanColorEl = document.querySelector('.color');
+
+const changeColorHandler = () => {
+  spanColorEl.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+btn.addEventListener('click',changeColorHandler)
