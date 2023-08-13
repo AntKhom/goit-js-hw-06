7bac6ab4cf92d3045a120c863321be5ad8c50c25
AntKhom <antonhomenko1988@gmail.com>
2023-08-13 13:23:35 -0400

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,12 @@
+const formInputEl = document.querySelector('#font-size-control');
+const formTextEl = document.querySelectorAll('#text');
+
+const changeFontSizeHandler = (e) => {
+    let textSize = e.currentTarget.value;
+    console.log(e.currentTarget.value);
+    console.log(textSize);
+
+    formTextEl.style.fontSize = `${textSize}px`;
+}
+
+formInputEl.addEventListener('change',changeFontSizeHandler)
